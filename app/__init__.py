1a33ca6b3220abb503287fd99dcbee6085074b93
meurveng <hernan.red.94@gmail.com>
2015-02-12 21:22:50 +0100

    Creada la estructura para utilizar Flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def home():
+    return render_template('home.html')  # return a string
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/ext')
+def welcome():
+    return render_template('ext.html')  # render a template
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
